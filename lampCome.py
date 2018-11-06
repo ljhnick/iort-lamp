b40901c0d79dbae9ef3292603af6a0ebbe1dc29b
ljhnick <40676897+ljhnick@users.noreply.github.com>
2018-11-05 18:04:14 -0800

    Add files via upload

--- lampCome.py
+++ lampCome.py
@@ -3,93 +3,94 @@ import sys, tty, termios
 
 from dynamixel_sdk import *
 
-def turnLamp(degree):
+# def turnLamp(degree):
                         # Uses Dynamixel SDK library
+degree = input()
+# Control table address
+ADDR_PRO_TORQUE_ENABLE      = 64               # Control table address is different in Dynamixel model
+ADDR_PRO_GOAL_POSITION      = 116
+ADDR_PRO_PRESENT_POSITION   = 132
+ADDR_PRO_PROFILE_VELOCITY   = 112
 
-    # Control table address
-    ADDR_PRO_TORQUE_ENABLE      = 64               # Control table address is different in Dynamixel model
-    ADDR_PRO_GOAL_POSITION      = 116
-    ADDR_PRO_PRESENT_POSITION   = 132
-    ADDR_PRO_PROFILE_VELOCITY   = 112
+# Protocol version
+PROTOCOL_VERSION            = 2.0               # See which protocol version is used in the Dynamixel
 
-    # Protocol version
-    PROTOCOL_VERSION            = 2.0               # See which protocol version is used in the Dynamixel
+# Default setting
+DXL_ID1                      = 1                 # Dynamixel ID : 1
+DXL_ID2                      = 2
+BAUDRATE                    = 57600             # Dynamixel default baudrate : 57600
+DEVICENAME                  = 'COM3'            # Check which port is being used on your controller
+                                                # ex) Windows: "COM1"   Linux: "/dev/ttyUSB0" Mac: "/dev/tty.usbserial-*"
 
-    # Default setting
-    DXL_ID1                      = 1                 # Dynamixel ID : 1
-    DXL_ID2                      = 2
-    BAUDRATE                    = 57600             # Dynamixel default baudrate : 57600
-    DEVICENAME                  = 'COM3'            # Check which port is being used on your controller
-                                                    # ex) Windows: "COM1"   Linux: "/dev/ttyUSB0" Mac: "/dev/tty.usbserial-*"
+TORQUE_ENABLE               = 1                 # Value for enabling the torque
+TORQUE_DISABLE              = 0                 # Value for disabling the torque
+DXL_MINIMUM_POSITION_VALUE  = 10           # Dynamixel will rotate between this value
+DXL_MAXIMUM_POSITION_VALUE  = 4000            # and this value (note that the Dynamixel would not move when the position value is out of movable range. Check e-manual about the range of the Dynamixel you use.)
+DXL_MOVING_STATUS_THRESHOLD = 20                # Dynamixel moving status threshold
 
-    TORQUE_ENABLE               = 1                 # Value for enabling the torque
-    TORQUE_DISABLE              = 0                 # Value for disabling the torque
-    DXL_MINIMUM_POSITION_VALUE  = 10           # Dynamixel will rotate between this value
-    DXL_MAXIMUM_POSITION_VALUE  = 4000            # and this value (note that the Dynamixel would not move when the position value is out of movable range. Check e-manual about the range of the Dynamixel you use.)
-    DXL_MOVING_STATUS_THRESHOLD = 20                # Dynamixel moving status threshold
-
-    index = 0
-    #dxl_goal_position = [DXL_MINIMUM_POSITION_VALUE, DXL_MAXIMUM_POSITION_VALUE]         # Goal position
-
-
-    # Initialize PortHandler instance
-    # Set the port path
-    # Get methods and members of PortHandlerLinux or PortHandlerWindows
-    portHandler = PortHandler(DEVICENAME)
-
-    # Initialize PacketHandler instance
-    # Set the protocol version
-    # Get methods and members of Protocol1PacketHandler or Protocol2PacketHandler
-    packetHandler = PacketHandler(PROTOCOL_VERSION)
-
-    # Open port
-    if portHandler.openPort():
-        print("Succeeded to open the port")
-    else:
-        print("Failed to open the port")
-        print("Press any key to terminate...")
-        getch()
-        quit()
+index = 0
+#dxl_goal_position = [DXL_MINIMUM_POSITION_VALUE, DXL_MAXIMUM_POSITION_VALUE]         # Goal position
 
 
-    # Set port baudrate
-    if portHandler.setBaudRate(BAUDRATE):
-        print("Succeeded to change the baudrate")
-    else:
-        print("Failed to change the baudrate")
-        print("Press any key to terminate...")
-        getch()
-        quit()
+# Initialize PortHandler instance
+# Set the port path
+# Get methods and members of PortHandlerLinux or PortHandlerWindows
+portHandler = PortHandler(DEVICENAME)
 
-    # Enable Dynamixel Torque ID = 1
-    dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID1, ADDR_PRO_TORQUE_ENABLE, TORQUE_ENABLE)
+# Initialize PacketHandler instance
+# Set the protocol version
+# Get methods and members of Protocol1PacketHandler or Protocol2PacketHandler
+packetHandler = PacketHandler(PROTOCOL_VERSION)
+
+# Open port
+if portHandler.openPort():
+    print("Succeeded to open the port")
+else:
+    print("Failed to open the port")
+    print("Press any key to terminate...")
+    getch()
+    quit()
+
+
+# Set port baudrate
+if portHandler.setBaudRate(BAUDRATE):
+    print("Succeeded to change the baudrate")
+else:
+    print("Failed to change the baudrate")
+    print("Press any key to terminate...")
+    getch()
+    quit()
+
+# Enable Dynamixel Torque ID = 1
+dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID1, ADDR_PRO_TORQUE_ENABLE, TORQUE_ENABLE)
+if dxl_comm_result != COMM_SUCCESS:
+    print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+elif dxl_error != 0:
+    print("%s" % packetHandler.getRxPacketError(dxl_error))
+else:
+    print("Dynamixel has been successfully connected")
+
+dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID2, ADDR_PRO_TORQUE_ENABLE, TORQUE_ENABLE)
+if dxl_comm_result != COMM_SUCCESS:
+    print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+elif dxl_error != 0:
+    print("%s" % packetHandler.getRxPacketError(dxl_error))
+else:
+    print("Dynamixel has been successfully connected")
+
+INTITIAL_POSITION = 0    # according to the sensor array
+dxl_goal_position = 3600 - (degree * 36.6)
+while 1:
+    dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, DXL_ID1, ADDR_PRO_GOAL_POSITION,
+                                                              dxl_goal_position)
     if dxl_comm_result != COMM_SUCCESS:
         print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
     elif dxl_error != 0:
         print("%s" % packetHandler.getRxPacketError(dxl_error))
-    else:
-        print("Dynamixel has been successfully connected")
 
-    dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID2, ADDR_PRO_TORQUE_ENABLE, TORQUE_ENABLE)
-    if dxl_comm_result != COMM_SUCCESS:
-        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-    elif dxl_error != 0:
-        print("%s" % packetHandler.getRxPacketError(dxl_error))
-    else:
-        print("Dynamixel has been successfully connected")
+    dxl_present_position, dxl_comm_result, dxl_error = packetHandler.read4ByteTxRx(portHandler, DXL_ID,
+                                                                                   ADDR_PRO_PRESENT_POSITION)
 
-    INTITIAL_POSITION = 0    # according to the sensor array
-    dxl_goal_position = 3600 - (degree * 36.6)
-    while 1:
-        dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, DXL_ID1, ADDR_PRO_GOAL_POSITION,
-                                                                  dxl_goal_position)
-        if dxl_comm_result != COMM_SUCCESS:
-            print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-        elif dxl_error != 0:
-            print("%s" % packetHandler.getRxPacketError(dxl_error))
+    if abs(dxl_present_position - dxl_goal_position) <= 50:
+        break
 
-        dxl_present_position, dxl_comm_result, dxl_error = packetHandler.read4ByteTxRx(portHandler, DXL_ID,
-                                                                                       ADDR_PRO_PRESENT_POSITION)
-
-        if abs(dxl_present_position - dxl_goal_position) <= 50:
-            break
